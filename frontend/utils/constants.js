e244bd3003e1f80d36e671dd98926bb8357c87dc
Obada-Safwa <safwaobada@gmail.com>
2025-06-08 13:12:36 +0200

    initialize real estate marketplace backend with core services and routes

--- frontend/utils/constants.js
+++
@@ -1,9 +0,0 @@
-var Constants = {
-  get_api_base_url: function () {
-    if (location.hostname == "localhost") {
-      return "http://localhost/Real-Estate-Marketplace/backend/";
-    } else {
-      return "https://walrus-app-dere7.ondigitalocean.app/Real-Estate-Marketplace/backend/";
-    }
-  },
-};
