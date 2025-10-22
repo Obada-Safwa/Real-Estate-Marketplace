b1260cdec521b21325634029a2dcc886829d6984
Obada-Safwa <safwaobada@gmail.com>
2025-10-22 18:04:56 +0200

    test

--- backend/rest/services/ReportsServices.php
+++ backend/rest/services/ReportsServices.php
@@ -18,8 +18,18 @@ class ReportsServices extends BaseServices
 
     public function add($entity)
     {
-        $property = $this->propertiesServices->get_by_id($entity['property_id']);
+        // Make sure property_id exists
+        if (empty($entity['property_id'])) {
+            throw new Exception("property_id is required to create a report");
+        }
 
+        // Fetch property details
+        $property = $this->propertiesServices->get_by_id($entity['property_id']);
+        if (empty($property) || empty($property['user_id'])) {
+            throw new Exception("Invalid property or property owner not found");
+        }
+
+        // Correctly add receiver_id
         $entity['receiver_id'] = $property['user_id'];
         return $this->dao->add($entity);
     }
