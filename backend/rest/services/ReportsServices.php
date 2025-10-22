458acfd473fd2566052425e267c3712c200b95d1
Obada-Safwa <safwaobada@gmail.com>
2025-10-22 17:53:28 +0200

    fix backend

--- backend/rest/services/ReportsServices.php
+++ backend/rest/services/ReportsServices.php
@@ -1,16 +1,26 @@
 <?php
-
 require_once "BaseServices.php";
+require_once "PropertiesServices.php";
 
 class ReportsServices extends BaseServices
 {
+    private $propertiesServices;
     public function __construct()
     {
         parent::__construct(new ReportsDao);
+        $this->propertiesServices = new PropertiesServices();
     }
 
     public function alter_status($status, $id)
     {
         return $this->dao->alter_status($status, $id);
+    }
+
+    public function add($entity)
+    {
+        $property = $this->propertiesServices->get_by_id($entity['property_id']);
+
+        $entity['receiver_id'] = $property['user_id'];
+        return $this->dao->add($entity);
     }
 }
